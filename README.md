b19aeebe3d7daf6517f09a4ef7b0d4a27d91d46b
PChadx <ChadTrivia@protonmail.com>
2023-02-08 11:28:20 +0000

    crypto vs math.random, update README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Testing Ground
-Testing different ideas of different weights.
+Testing different ideas of different weights, sometimes they end up here.
+
+## Id based captcha
+Was interested in the crypto module where you would generate something like captcha based on user id.
+Very slow (2ms on this machine) - as expected - compared to Math.random
--- createCaptcha.js
+++ createCaptcha.js
@@ -1,13 +1,15 @@
-const crypto = require('crypto');
+//const crypto = require('crypto');
+const Math = require('math');
 
 const createCaptcha = (userId) => {
-    startTime = Date.now();
-    const seed = userId.toString();
-    
-    const captchaValue = crypto.createHash('sha256').update(seed).digest('hex').substring(0, 6);
+  startTime = Date.now();
+  //const seed = userId.toString();
   
-    console.log(`All done! It took me ${Date.now() - startTime} ms`);
-    return captchaValue;
-  };
-  
-  module.exports = { createCaptcha };
+  //const captchaValue = crypto.createHash('sha256').update(seed).digest('hex').substring(0, 6);
+  const captchaValue = Math.floor(Math.random() * (1000000 - 100000) + 100000).toString();
+
+  console.log(`All done! It took me ${Date.now() - startTime} ms`);
+  return captchaValue;
+};
+
+module.exports = { createCaptcha };
